17174f810d302390068701adc3bc6671b84fedc3
Rahim <kristopherab@gmail.com>
2025-08-24 20:42:05 +0100

    echo

---
+++ cmd/protohackers/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	fatal(err)
+
+	fatal(serve(ln))
+}
+
+func serve(ln net.Listener) error {
+	defer ln.Close()
+	for {
+		rwc, err := ln.Accept()
+		if err != nil {
+			return fmt.Errorf("failed to accept: %w", err)
+		}
+
+		go handle(rwc)
+	}
+}
+
+func handle(rwc net.Conn) {
+	defer rwc.Close()
+
+	for {
+		if _, err := io.Copy(rwc, rwc); err != nil {
+			// todo...
+			return
+		}
+	}
+}
+
+func fatal(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
