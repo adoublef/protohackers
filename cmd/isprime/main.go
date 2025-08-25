6142ccd07510d8cf357011cd5808923490311dcc
Rahim <kristopherab@gmail.com>
2025-08-25 16:54:56 +0100

    use encoder

--- cmd/isprime/main.go
+++ cmd/isprime/main.go
@@ -29,8 +29,9 @@ func serve(ln net.Listener) error {
 func handle(rwc net.Conn) {
 	defer rwc.Close()
 
-	sc := bufio.NewScanner(rwc)
+	enc := json.NewEncoder(rwc)
 
+	sc := bufio.NewScanner(rwc)
 	for sc.Scan() {
 		var p struct {
 			Method string       `json:"method"`
@@ -42,11 +43,11 @@ func handle(rwc net.Conn) {
 			break
 		}
 		if p.Method != "isPrime" || p.Number == nil {
-			log.Printf("malformed payload %v", p)
+			// log.Printf("malformed payload %v", p)
 			fmt.Fprintf(rwc, "MALFORMED\n")
 			break
 		}
-		log.Printf("ok payload %v", p)
+		log.Printf("payload %v", p)
 
 		var v struct {
 			Method string `json:"method"`
@@ -56,21 +57,23 @@ func handle(rwc net.Conn) {
 
 		n, err := p.Number.Int64()
 		if err != nil {
-			p, _ := json.Marshal(v)
-			log.Printf("float response %q", string(p))
-			rwc.Write([]byte(string(p) + "\n"))
+			// b, _ := json.Marshal(v)
+			// log.Printf("float response %q", string(p))
+			// rwc.Write([]byte(string(b) + "\n"))
+			enc.Encode(v)
 			break
 		}
 
 		if isPrime(int(n)) {
 			v.Prime = true
 		}
-		b, _ := json.Marshal(v)
-		log.Printf("ok response %q", string(b))
-		rwc.Write([]byte(string(b) + "\n"))
+		enc.Encode(v)
+		// b, _ := json.Marshal(v)
+		// log.Printf("ok response %q", string(b))
+		// rwc.Write([]byte(string(b) + "\n"))
 	}
 	if err := sc.Err(); err != nil {
-		log.Printf("scan error: %v", err)
+		// log.Printf("scan error: %v", err)
 		fmt.Fprintf(rwc, "MALFORMED\n")
 	}
 }
