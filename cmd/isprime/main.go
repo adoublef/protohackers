3b2d748bdbcf4a34ef9e02ad1dee1961bd37a180
Rahim <kristopherab@gmail.com>
2025-08-25 15:54:38 +0100

    kinda works

---
+++ cmd/isprime/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math"
+	"net"
+
+	"go.adoublef.dev/runtime/debug"
+)
+
+func main() {
+	ln, _ := net.Listen("tcp", "0.0.0.0:8080")
+	serve(ln)
+}
+
+func serve(ln net.Listener) error {
+	for {
+		rwc, err := ln.Accept()
+		if err != nil {
+			return err
+		}
+
+		go handle(rwc)
+	}
+}
+
+func handle(rwc net.Conn) {
+	defer rwc.Close()
+
+	var p struct {
+		Method string
+		Number *int
+	}
+	if err := json.NewDecoder(rwc).Decode(&p); err != nil {
+		fmt.Fprintf(rwc, "MALFORMED\n")
+		return
+	}
+	if p.Method != "isPrime" || p.Number == nil {
+		fmt.Fprintf(rwc, "MALFORMED\n")
+		return
+	}
+
+	var v struct {
+		Method string `json:"method"`
+		Prime  bool   `json:"prime"`
+	}
+	v.Method = p.Method
+	e := json.NewEncoder(rwc)
+	if isPrime(*p.Number) {
+		v.Prime = true
+	}
+	_ = e.Encode(v)
+}
+
+func isPrime(n int) bool {
+	for i := 2; i <= int((math.Sqrt(float64(n)))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return n > 1
+}
---
+++ cmd/isprime/main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"go.adoublef.dev/testing/is"
+)
+
+func Test_isPrime(t *testing.T) {
+	// run server
+	ln, err := net.Listen("tcp", "")
+	is.OK(t, err) // net.Listen
+
+	go serve(ln)
+	t.Cleanup(func() { ln.Close() })
+
+	conn, err := net.Dial("tcp", ln.Addr().String())
+	is.OK(t, err) // net.Dial
+	defer conn.Close()
+
+	_, err = conn.Write([]byte(`{"method":"isPrime","number":1}`))
+	is.OK(t, err)
+
+	// read response
+	buf := make([]byte, 1024)
+	conn.SetReadDeadline(time.Now().Add(2 * time.Second)) // avoid hanging forever
+	n, err := conn.Read(buf)
+	is.OK(t, err) // Read
+
+	t.Logf("%s", string(buf[:n]))
+}
