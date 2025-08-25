230a3e8f9bcc9f94fd0810e6a69885e6d2d86dbf
Rahim <kristopherab@gmail.com>
2025-08-25 16:20:38 +0100

    for loop

--- cmd/isprime/main.go
+++ cmd/isprime/main.go
@@ -27,43 +27,45 @@ func serve(ln net.Listener) error {
 func handle(rwc net.Conn) {
 	defer rwc.Close()
 
-	var p struct {
-		Method string       `json:"method"`
-		Number *json.Number `json:"number"`
-	}
-	if err := json.NewDecoder(rwc).Decode(&p); err != nil {
-		log.Printf("malformed payload %v", p)
-		fmt.Fprintf(rwc, "MALFORMED\n")
-		return
-	}
-	log.Printf("input payload %v", p)
-	if p.Method != "isPrime" || p.Number == nil {
-		fmt.Fprintf(rwc, "MALFORMED\n")
-		return
-	}
+	for {
+		var p struct {
+			Method string       `json:"method"`
+			Number *json.Number `json:"number"`
+		}
+		if err := json.NewDecoder(rwc).Decode(&p); err != nil {
+			log.Printf("malformed payload %v", p)
+			fmt.Fprintf(rwc, "MALFORMED\n")
+			return
+		}
+		log.Printf("input payload %v", p)
+		if p.Method != "isPrime" || p.Number == nil {
+			fmt.Fprintf(rwc, "MALFORMED\n")
+			return
+		}
 
-	// {"method":"isPrime","prime":false}
-	var v struct {
-		Method string `json:"method"`
-		Prime  bool   `json:"prime"`
-	}
-	v.Method = p.Method
+		// {"method":"isPrime","prime":false}
+		var v struct {
+			Method string `json:"method"`
+			Prime  bool   `json:"prime"`
+		}
+		v.Method = p.Method
 
-	n, err := p.Number.Int64()
-	log.Printf("input %d", n)
-	if err != nil {
-		p, _ := json.Marshal(v)
-		log.Printf("float response %q", string(p))
-		rwc.Write([]byte(string(p) + "\n"))
-		return
-	}
+		n, err := p.Number.Int64()
+		log.Printf("input %d", n)
+		if err != nil {
+			p, _ := json.Marshal(v)
+			log.Printf("float response %q", string(p))
+			rwc.Write([]byte(string(p) + "\n"))
+			return
+		}
 
-	if isPrime(int(n)) {
-		v.Prime = true
+		if isPrime(int(n)) {
+			v.Prime = true
+		}
+		b, _ := json.Marshal(v)
+		log.Printf("ok response %q", string(b))
+		rwc.Write([]byte(string(b) + "\n"))
 	}
-	b, _ := json.Marshal(v)
-	log.Printf("ok response %q", string(b))
-	rwc.Write([]byte(string(b) + "\n"))
 }
 
 func isPrime(n int) bool {
