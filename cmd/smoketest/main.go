9cf9311a79520c9b32a021db6bc3f50fb1a3fcbc
Rahim <kristopherab@gmail.com>
2025-08-25 16:45:45 +0100

    st

--- cmd/smoketest/main.go
+++ cmd/smoketest/main.go
@@ -1,38 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"net"
 )
 
 func main() {
-	ln, err := net.Listen("tcp", "0.0.0.0:8080")
-	fatal(err)
-
-	fatal(serve(ln))
-}
-
-func serve(ln net.Listener) error {
-	defer ln.Close()
+	ln, _ := net.Listen("tcp", "0.0.0.0:8080")
 	for {
-		rwc, err := ln.Accept()
-		if err != nil {
-			return fmt.Errorf("failed to accept: %w", err)
-		}
-
-		go handle(rwc)
-	}
-}
-
-func handle(rwc net.Conn) {
-	defer rwc.Close()
-
-	_, _ = io.Copy(rwc, rwc)
-}
-
-func fatal(err error) {
-	if err != nil {
-		panic(err)
+		rwc, _ := ln.Accept()
+		go io.Copy(rwc, rwc)
 	}
 }
